37e6dbe68d1040e0c079e52d9e2fa23e729e5840
Ananias <amychelleoropezaortiz@gmail.com>
2023-12-21 20:16:12 -0500

    Agregado los colores y personalización de fondo con imagen dependiendo del genero.

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,31 @@ let usuarios = [
     {
         Nombre: "Ananias",
         Apellido: "Oropeza",
+        Genero:"Masculino",
         Email: "amychelleoropezaortiz@gmail.com",
         Password: "nani190905"
     },
     {
         Nombre: "Jeremy",
         Apellido: "Soto",
+        Genero:"Masculino",
         Email: "soto.vega@gmail.com",
         Password: "umai!"
-    }
+    },
+    {
+        Nombre: "Laura/Lucian",
+        Apellido: "Restrepo",
+        Genero:"Fluido",
+        Email: "restrepoLauris@gmail.com",
+        Password: "lau12@"
+    },
+    {
+        Nombre: "Adriana",
+        Apellido: "Baez",
+        Genero:"Femenino",
+        Email:"lulubox@hotmail.com",
+        Password: "kiguri229"
+    },
 ];
 
 let btnIngreso = document.getElementById("btnIngreso");
@@ -26,10 +42,9 @@ btnIngreso.addEventListener("click", () => {
         if (email === usuario.Email && password === usuario.Password) {
             usuarioEncontrado = true;
             localStorage.setItem('Nombre', usuario.Nombre);
+            localStorage.setItem('Genero', usuario.Genero);
             location.href = "ingresado.html";
-        }else if(email == '' || password == ''){
-        
-        }
+        };
     });
 
     if (!usuarioEncontrado) {
--- JS/scriptSalida.js
+++ JS/scriptSalida.js
@@ -1,13 +1,37 @@
 let btnSalir = document.getElementById("btnSalir");
 let personita = document.getElementById("personita");
+let imagen = document.getElementById("imagen");
 
 let nombre = localStorage.getItem("Nombre");
+let genero = localStorage.getItem("Genero");
 
 if (nombre == null){
     location.href = "index.html";
 } else {
     personita.innerHTML = nombre;
 }
+
+let imageCaminito;
+
+if (genero === "Masculino") {
+    adentro.style.background = 'url("Img/Masculino.png")';
+    imageCaminito = 'Img/dancing.jpg';
+} else if (genero === "Femenino") {
+    adentro.style.background = 'url("Img/Femenino.png")';
+    imageCaminito = 'Img/wearefuck.jpg';
+} else if (genero === "Fluido") {
+    adentro.style.background = 'url("Img/Fluido.png")';
+    imageCaminito = 'Img/Ups.jpg';
+}
+
+let img = document.createElement('img');
+img.src = imageCaminito;
+img.alt = 'Ayuda';
+img.style.width = "300px";
+img.style.height = "300px";
+img.style.margin = "10px";
+img.style.border = "solid";
+adentro.appendChild(img);
 
 btnSalir.addEventListener("click", () => {
     localStorage.clear()
